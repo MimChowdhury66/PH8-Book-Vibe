8362c45df348213fec62fec00a2dfdb9ea09f4cc
mim chowdhury <umac3333@gmail.com>
2024-03-26 22:28:16 +0600

    new-section-added

--- src/components/NewBook/NewBook.jsx
+++ src/components/NewBook/NewBook.jsx
@@ -4,13 +4,13 @@ import { MdCategory } from "react-icons/md";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const NewBook = ({ newBook }) => {
-    const { bookName, author, image, rating, category, publisher } = newBook;
+    const { bookName, author, image, rating, category, publisher,yearOfPublishing } = newBook;
 
     const handleBtn = () => toast("Successsfully added to the cart.");
     return (
         < div className="card card-side bg-base-100 shadow-xl" >
 
-            <figure><img src={image} alt="Movie" /></figure>
+            <figure><img src={image} alt="Book" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{bookName}</h2>
                 <p>Written By: {author}
@@ -24,7 +24,11 @@ const NewBook = ({ newBook }) => {
                         Publisher: {publisher}</p>
                 </div>
                 <hr />
+                <div className="flex">
                 <p className="flex items-center gap-2 text-base font-medium"><CiStar />{rating}</p>
+                <p>Published Year: { yearOfPublishing}</p>
+
+                </div>
                 <div className="card-actions justify-end">
                     <button onClick={handleBtn} className="btn bg-[#23BE0A] text-white">Buy Now</button>
                     <ToastContainer />
